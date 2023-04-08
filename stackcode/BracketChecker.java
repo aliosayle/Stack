89a8df203e9ec425e0bbccf17aa61fcb3eb3ef0b
aliosseili <aliossaily24@gmail.com>
2023-04-08 13:49:11 +0300

    commented the code

--- stackcode/BracketChecker.java
+++ stackcode/BracketChecker.java
@@ -4,22 +4,22 @@ public class BracketChecker {
     public static boolean check(String expression) {
         Stack stack = new Stack();
         
-        for (int i = 0; i < expression.length(); i++) {
+        for (int i = 0; i < expression.length(); i++) { //we create a loop that will iterate between all the characters of the String
             char c = expression.charAt(i);
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else if (c == ')' || c == '}' || c == ']') {
+            if (c == '(') {
+                stack.push(c); //we add the opening brackets to the stack
+            } else if (c == ')') {
                 if (stack.isEmpty()) {
-                    return false;
+                    return false; //return false if the stack has no opening brackets and it finds a closing bracket
                 }
-                char top = stack.pop();
-                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
-                    return false;
+                char top = stack.pop(); //if stack is not empty it means the two brackets are balanced, we pop them
+                if (c == ')' && top == ')') {
+                    return false; //return false if they don't match
                 }
             }
         }
         
-        return stack.isEmpty();
+        return stack.isEmpty(); //if the stack is empty it means that all the brackets cancel each other out and are balanced
     }
     
     public static void main(String[] args) {
